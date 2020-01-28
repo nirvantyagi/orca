d2139bfdee1d94284082ee459963719a7eb0a9ec
Nirvan Tyagi <nirvan.tyagi@gmail.com>
2020-01-27 21:11:22 -0500

    Switch algebraic mac to projective curve group in order to serialize with affine element

--- src/algmac.rs
+++ src/algmac.rs
@@ -1,28 +1,28 @@
 use crate::Error;
 use algebra::{
     bytes::ToBytes,
-    fields::PrimeField,
+    curves::ProjectiveCurve,
     groups::Group,
+    fields::PrimeField,
     to_bytes, UniformRand,
 };
 use digest::Digest;
 use rand::Rng;
 use std::{
-    hash::Hash,
     io::{Result as IoResult, Write},
     marker::PhantomData,
     vec::Vec,
 };
 
-pub struct AlgMac<G: Group, D: Digest>(PhantomData<G>, PhantomData<D>);
+pub struct AlgMac<G: ProjectiveCurve, D: Digest>(PhantomData<G>, PhantomData<D>);
 
-pub struct PublicParams<G: Group> {
+pub struct PublicParams<G: ProjectiveCurve> {
     g: G,
     h: G,
 }
 
 //TODO: derive algebra::bytes::ToBytes from struct?
-impl<G: Group> ToBytes for PublicParams<G> {
+impl<G: ProjectiveCurve> ToBytes for PublicParams<G> {
     fn write<W: Write>(self: &Self, mut writer: W) -> IoResult<()> {
         self.g.write(&mut writer)?;
         self.h.write(&mut writer)
@@ -31,12 +31,12 @@ impl<G: Group> ToBytes for PublicParams<G> {
 
 // Public and private MAC key pair
 #[derive(Clone)]
-pub struct PubKey<G: Group> {
+pub struct PubKey<G: ProjectiveCurve> {
     CX0: G,
     X1: G,
 }
 
-impl<G: Group> ToBytes for PubKey<G> {
+impl<G: ProjectiveCurve> ToBytes for PubKey<G> {
     fn write<W: Write>(self: &Self, mut writer: W) -> IoResult<()> {
         self.CX0.write(&mut writer)?;
         self.X1.write(&mut writer)
@@ -44,26 +44,26 @@ impl<G: Group> ToBytes for PubKey<G> {
 }
 
 
-pub struct SecretKey<G: Group> {
+pub struct SecretKey<G: ProjectiveCurve> {
     x0: G::ScalarField,
     x1: G::ScalarField,
     xt: G::ScalarField,
     pk: PubKey<G>,
 }
 
-pub struct Mac<G: Group> {
+pub struct Mac<G: ProjectiveCurve> {
     u0: G,
     u1: G,
 }
 
-impl<G: Group> ToBytes for Mac<G> {
+impl<G: ProjectiveCurve> ToBytes for Mac<G> {
     fn write<W: Write>(self: &Self, mut writer: W) -> IoResult<()> {
         self.u0.write(&mut writer)?;
         self.u1.write(&mut writer)
     }
 }
 
-pub struct MacProof<G: Group, D: Digest> {
+pub struct MacProof<G: ProjectiveCurve, D: Digest> {
     z0: G::ScalarField,
     z1: G::ScalarField,
     zt: G::ScalarField,
@@ -71,7 +71,7 @@ pub struct MacProof<G: Group, D: Digest> {
     phantom: PhantomData<D>,
 }
 
-impl<G: Group + Hash, D: Digest> AlgMac<G, D> {
+impl<G: ProjectiveCurve, D: Digest> AlgMac<G, D> {
     pub fn setup<R: Rng>(gen: &G, rng: &mut R) -> PublicParams<G> {
         PublicParams {
             g: gen.mul(&G::ScalarField::rand(rng)),
@@ -126,7 +126,7 @@ impl<G: Group + Hash, D: Digest> AlgMac<G, D> {
     ) -> Result<(Mac<G>, MacProof<G, D>), Error>{
         let t = Self::blind_mac(pp, sk, M);
         // Generate random commitments
-        let (r0, r1, rt, c, s_u1) = loop {
+        let (r0, r1, rt, c) = loop {
             let r0 = G::ScalarField::rand(rng);
             let r1 = G::ScalarField::rand(rng);
             let rt = G::ScalarField::rand(rng);
@@ -137,13 +137,18 @@ impl<G: Group + Hash, D: Digest> AlgMac<G, D> {
             // Hash statement and commitments to get challenge
             // TODO: Does hash function create random bytes that maps to full scalar field?
             let mut hash_input = Vec::new();
-            //let hash_bytes = to_bytes![pp, sk.pk, t, M, s_u1, s_cx0, s_x1]?;
-            let hash_bytes = to_bytes![s_u1]?;
-            println!("P u1 commitment: {}", s_u1);
-            println!("P hash bytes: {:x?}", hash_bytes);
+            let hash_bytes = to_bytes![
+                pp,
+                sk.pk,
+                t,
+                M,
+                s_u1.into_affine(),
+                s_cx0.into_affine(),
+                s_x1.into_affine()
+            ]?;
             hash_input.extend_from_slice(&hash_bytes);
             if let Some(c) = G::ScalarField::from_random_bytes(&D::digest(&hash_input)) {
-                break (r0, r1, rt, c, s_u1);
+                break (r0, r1, rt, c);
             };
         };
 
@@ -155,12 +160,6 @@ impl<G: Group + Hash, D: Digest> AlgMac<G, D> {
             c: c,
             phantom: PhantomData,
         };
-
-        let v_s_u1 = t.u0.mul(&proof.z0) + M.mul(&proof.z1) - &t.u1.mul(&proof.c);
-        let hash_bytes = to_bytes![v_s_u1]?;
-        println!("V u1 commitment: {}", v_s_u1);
-        println!("V hash bytes: {:x?}", hash_bytes);
-        println!("P == V? {}", s_u1 == v_s_u1);
 
         Ok((t, proof))
     }
@@ -177,25 +176,26 @@ impl<G: Group + Hash, D: Digest> AlgMac<G, D> {
         let s_x1 = pp.h.mul(&proof.z1) - &pk.X1.mul(&proof.c);
 
         let mut hash_input = Vec::new();
-        //let hash_bytes = to_bytes![pp, pk, t, M, s_u1, s_cx0, s_x1]?;
-        let hash_bytes = to_bytes![s_u1]?;
-        println!("C u1 commitment: {}", s_u1);
-        println!("C hash bytes: {:x?}", hash_bytes);
+        let hash_bytes = to_bytes![
+            pp,
+            pk,
+            t,
+            M,
+            s_u1.into_affine(),
+            s_cx0.into_affine(),
+            s_x1.into_affine()
+        ]?;
         hash_input.extend_from_slice(&hash_bytes);
         match G::ScalarField::from_random_bytes(&D::digest(&hash_input)) {
             None => Ok(false),
-            Some(c) => {
-                println!("Calculated challenge: {}", c);
-                println!("Proof challenge: {}", proof.c);
-                Ok(c == proof.c)
-            },
+            Some(c) => Ok(c == proof.c),
         }
     }
 
 
 }
 
-impl<G: Group> Mac<G> {
+impl<G: ProjectiveCurve> Mac<G> {
     pub fn rerandomize<R: Rng>(self: &Self, rng: &mut R) -> Self {
         let r = G::ScalarField::rand(rng);
         Mac {
@@ -243,7 +243,6 @@ mod tests {
         let m = Fr::rand(&mut rng);
         let M = pp.g.mul(&m);
         let (t, proof) = AlgMacG1::blind_mac_and_prove(&pp, &sk, &M, &mut rng).unwrap();
-        println!("Output of verify: {}", AlgMacG1::verify_mac_proof(&pp, &pk, &M, &t, &proof).unwrap());
         assert!(AlgMacG1::verify_mac_proof(&pp, &pk, &M, &t, &proof).unwrap());
     }
 
